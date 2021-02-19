2ab1731d3b87dfe089e117150f920263b5f32e7a
Tendai Nyandoro <tendai1@hotmail.com>
2021-02-19 16:25:52 +0200

    Add Events todisplay the book
    Add form value method

--- scripts/main.js
+++ scripts/main.js
@@ -21,6 +21,45 @@ function Book(author, title, pages, read, isbn) {
 // Sore Class: Handle local storage
 
 // Events: Display the books
+
+document.querySelector('#book-form').addEventListener('submit', 
+function(e){
+  // lets get the form values
+
+  const author = document.querySelector('#author').value;
+  const title = document.querySelector('#title').value;
+  const pages = document.querySelector('#pages').value;
+  const isbn = document.querySelector('#isbn').value;
+
+  // lets instatiate a book
+  const book = new Book(author, title, pages, isbn);
+
+  // Instantiate UI
+  const ui = new UI();
+
+  //Validatition
+  if(author === '' || title === '' || pages || '' || isbn === ''){
+
+    // Error Alert
+
+    ui.showAlert('Please fill in fields', 'error');
+  }else{
+    //Add Book To The List
+
+    ui.addBookToList(book);
+
+    //Show Success msg
+
+    ui.showAlert('Book Added', 'success');
+
+     //Clear Fields
+
+     ui.clearfields();
+  }
+
+  e.preventDefault();
+
+})
 // Event to Add a book 
 // event to remove a book
 
