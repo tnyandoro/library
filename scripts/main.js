c84d58f17390e191271e713573f9ba36e2d7c31b
Tendai Nyandoro <tendai1@hotmail.com>
2021-02-19 15:45:12 +0200

    Add more fields for the book

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+let myLibray = [];
+
+
+function Book(author, title, pages, read, isbn) {
+  this.author = author;
+  this.title = title;
+  this.page = pages;  
+  this.read = read;
+  this.isbn = isbn;
+}
+
+
+
+
+// Book function
+
+
+
+// UI class: Deal with the UI Tasks
+
+// Sore Class: Handle local storage
+
+// Events: Display the books
+// Event to Add a book 
+// event to remove a book
+
