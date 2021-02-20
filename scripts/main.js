d0a0adb45656cd60167b1d3f96e3d8d9e315add1
Tendai Nyandoro <tendai1@hotmail.com>
2021-02-20 14:52:27 +0200

    Add function to display the book list

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,12 @@
 /* eslint-disable max-classes-per-file */
 // Book Class for the book
 class Book {
-  constructor(title, author, isbn) {
+  constructor(title, author, isbn, pages, read) {
     this.title = title;
     this.author = author;
     this.isbn = isbn;
+    this.pages = pages;
+    this.read = read;
   }
 }
 
@@ -17,12 +19,14 @@ class UI {
         author: 'John Terry',
         isbn: '784654',
         pages: '365',
+        read: true,
       },
       {
-        title: 'HTML Basics',
-        author: 'John Terry',
+        title: 'JavaScript Basics',
+        author: 'Ivan Kyicv',
         isbn: '784654',
-        pages: '365',
+        pages: '9887365',
+        read: true,
       },
     ];
 
@@ -33,139 +37,30 @@ class UI {
 
   static addBookToList(book) {
     const list = document.querySelector('#book-list');
+
+    // Lets create a table row element
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.isbn}</td>
+      <td>${book.pages}</td>
+      <td>${book.read}</td>
+      <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+    `;
+
+    list.appendChild(row);
   }
 }
 
 // Store Class: Handles Storage local
 
 
-// Events : Display Books
+// Events : Display Books listen to the event and the call the even to load books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
-// Event: add a book
+// Event: add a Book 
+document.querySelector('#book-form')
 
-// Event:  Remove a book
-
-
-/* eslint-disable max-classes-per-file */
-// eslint-disable-next-line no-unused-vars
-// eslint-disable-next-line max-classes-per-file
-// eslint-disable-next-line no-unused-vars
-// const BookList = [];
-
-// class Book {
-//   constructor(author, title, pages, read, isbn) {
-//     this.author = author;
-//     this.title = title;
-//     this.pages = pages;
-//     this.read = read;
-//     this.isbn = isbn;
-//   }
-// }
-
-// class UI {
-//   // eslint-disable-next-line class-methods-use-this
-//   addBookToList(book) {
-//     const list = document.querySelector('#book-list');
-//     const row = document.createElement('tr');
-//     row.innerHTML = `
-
-//     <td>${book.author}</td>
-//     <td>${book.title}</td>
-//     <td>${book.pages}</td>
-//     <td>${book.read}</td>
-//     <td>${book.isbn}</td>
-//     <td><a href="" class="delete btn btn-danger">X</td>
-
-//     `;
-//     list.appendChild(row);
-//   }
-
-//   // eslint-disable-next-line class-methods-use-this
-//   showAlert(message, className) {
-//     const div = document.createElement('div');
-
-//     // Add the className
-
-//     div.className = `alert ${className}`;
-
-//     div.appendChild(document.createTextNode(message));
-
-//     const container = document.querySelector('.container');
-
-//     // Get Form
-
-//     const form = document.querySelector('#book-form');
-
-//     // Put alert
-
-//     container.insertBefore(div, form);
-
-//     // Timeout after 3 seconds
-//     setTimeout(() => {
-//       document.querySelector('.alert').remove();
-//     }, 3000);
-//   }
-
-//   // eslint-disable-next-line class-methods-use-this
-//   deleteBook(target) {
-//     if (target.className === 'delete') {
-//       target.parentElement.parentElement.remove();
-//     }
-//   }
-
-//   // eslint-disable-next-line class-methods-use-this
-//   clearFields() {
-//     document.querySelector('#author').value = '';
-//     document.querySelector('#title').value = '';
-//     document.querySelector('#pages').value = '';
-//     document.querySelector('#read').value = '';
-//     document.querySelector('#isbn').value = '';
-//   }
-// }
-
-// // Event Listening
-
-//    document.getElementById('book-form').addEventListener('submit', function (e) {
-
-//     //Lets Get Forms
-
-//     const author = document.getElementById('author').value
-//     const title = document.getElementById('title').value
-//     const read = document.getElementById('read').value
-//     const isbn = document.getElementById('isbn').value
-//    })
-
-//     // lets instatiate a book
-//     const book = new Book(author, title, pages, read, isbn);
-
-//     // Instantiate UI
-//     const ui = new UI();
-
-//     // Validatition
-//     if (author === '' || title === '' || pages === '' || read === ''|| isbn === '') {
-
-//       // Error Alert
-
-//       ui.showAlert('Please fill in fields', 'error');
-//     } else {
-//     // Add Book To The List
-
-//       ui.addBookToList(book);
-
-//       // Show Success msg
-
-//       ui.showAlert('Book Added', 'success');
-
-//       // Clear Fields
-
-//       ui.clearFields();
-//     }
-
-//     e.preventDefault();
-//   });
-
-// // event to remove a book
-// document.querySelector('#book-list').addEventListener('click',
-//   () => {
-
-//   });
+// Event: Remove a book
